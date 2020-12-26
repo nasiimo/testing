4bd316a5b90e9bbc75171c3e2571d71ba8a1a774
Waqar <nasiimo@venora.no>
2020-12-26 21:27:29 +0100

    first commit

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <h1>Im the login page</h1>
+      <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify-logo"></img>
+    </div>
+  );
+}
+
+export default Login
